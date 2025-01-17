eb2b35e611cf2502b0bdcb3e4bb22c293c97dbee
maxime nogues <maxime.nogues1408@gmail.com>
2025-01-17 17:03:25 +0100

    Got the bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, uniqueness { scope: :list_id, message: "It's unique and working!"}
+  validates :movie_id, uniqueness: { scope: :list_id, message: "It's unique and working!"}
   validates :comment, length: { minimum: 6 }
 end
