018e62746f461d68c8f3cb643c9c6e574f2ae9df
maxime nogues <maxime.nogues1408@gmail.com>
2025-01-17 15:15:03 +0100

    Code cleaned up

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 #   end
 
 puts "Cleaning database..."
-Movie.destroy
-List.destroy
+Movie.destroy_all
+List.destroy_all
 
 puts "Create movies and lists DB..."
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
