c74a6d0870382f81972def319e8209fecf469a36
maxime nogues <maxime.nogues1408@gmail.com>
2025-01-17 16:27:48 +0100

    Done again using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:create, :index, :new, :show]
+  resources :lists, only: [:create, :index, :new, :show] do
+    resources :bookmarks, only: [:create, :new]
+  end
+  resources :bookmarks, only: [:destroy]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -8,20 +11,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  # User can see all the lists
-  get "lists", to: "lists#index"
-
-  # User can create a movie
-  get "lists/new", to: "lists#new"
-  post "lists", to: "lists#create"
-
-  # User can see the details of a movie
-  get "lists/:id", to: "lists#show", as: :list
-
-  # BOOKMARKS
-  get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_list_bookmark
-  post "lists/:id/bookmarks", to: "bookmarks#create", as: :list_bookmark
-
-  delete "bookmarks/:id", to: "bookmarks#destroy", as: :bookmark
-
 end
